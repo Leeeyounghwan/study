00f4fab218f571c91473060bd1c9acd9907d1dfd
Leeeyounghwan <lee.yhwan226@gmail.com>
2023-05-14 17:17:28 +0900

    23.05.14 17:17 / 3052번 성공

--- src/main/java/week01/_3052_.java
+++ src/main/java/week01/_3052_.java
@@ -11,7 +11,7 @@ public class _3052_ {
         // HashSet 사용방법
         // HashSet 은 자바 Collection 중 Set 의 파생클래스이다.
         // HashSet은 중복되는 원소를 넣을 경우 하나만 저장한다. --> 중복원소를 허용하지 않는다.
-        // HashSet 은 순서 개념이 없다.
+        // HashSet 은 순서 개념이 없다.공
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> hs = new HashSet<Integer>();
