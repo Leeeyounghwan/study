6d2169556749cb1d9f96682d5257a567e86f2622
Leeeyounghwan <lee.yhwan226@gmail.com>
2023-05-14 17:13:03 +0900

    23.05.14 17:11 / 3052번 성공

--- src/main/java/week01/_3052_.java
+++ src/main/java/week01/_3052_.java
@@ -8,6 +8,11 @@ import java.util.HashSet;
 public class _3052_ {
     public static void main(String[] args) throws IOException {
 
+        // HashSet 사용방법
+        // HashSet 은 자바 Collection 중 Set 의 파생클래스이다.
+        // HashSet은 중복되는 원소를 넣을 경우 하나만 저장한다. --> 중복원소를 허용하지 않는다.
+        // HashSet 은 순서 개념이 없다.
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         HashSet<Integer> hs = new HashSet<Integer>();
 
